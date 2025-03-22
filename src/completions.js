76aaa43ef8d28e3d59b8ec5b73b3f717c2d8defc
Yanshuo Chu <yanshuochu@qq.com>
2025-03-22 10:57:23 +0800

    change to summary

--- src/completions.js
+++ src/completions.js
@@ -81,7 +81,7 @@ export async function getCompletion({ articles }) {
 
     const articlesWithResponses = articlesTrimed.map((article, index) => ({
         ...article,
-        response: responses[index].response
+        summary: responses[index].response
     }));
 
     return articlesWithResponses;
